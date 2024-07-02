c70f3b5e4c64b3456508a1a7a5cee6ccda089a84
Saksham Jain <sakshamj106@gmail.com>
2024-07-02 17:17:39 +0530

    register and login auth done

---
+++ api/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/posts",postRoute);
+app.use("/api/auth",authRoute);
+
+app.listen(8800, () => {
+    console.log("server is running!!");
+})
