c0b3da0b69652b4599ec3487be274bb247cd0d92
Saksham Jain <sakshamj106@gmail.com>
2024-07-04 00:08:02 +0530

    login and register backend done

--- api/app.js
+++ api/app.js
@@ -3,10 +3,12 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
-
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true}))
 app.use(express.json());
 app.use(cookieParser());
 
